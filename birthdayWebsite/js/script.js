9be33bdb677c771e5fac4285d2c301d0308296be
Koushik Rai <123098956+koushikrai@users.noreply.github.com>
2023-12-14 21:00:18 +0530

    birthdayWishing with photoGallery

---
+++ birthdayWebsite/js/script.js
@@ -0,0 +1,21 @@
+//trasition on second  page letters
+$('.imageGallery').flipster({
+  style:'carousel',
+  spacing:-0.3,
+})
+
+
+var typed = new Typed("#element", {
+  strings: [
+    "<i>Daughter</i>",
+    "<i>Sister</i>",
+    "<i>Friend</i>",
+    "<i>Person</i>",
+    "<i>Soul</i>",
+  ],
+  typeSpeed: 80,
+  
+  
+
+});
+string.prototype.repeat(5);
